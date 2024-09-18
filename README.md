89aa7745290b56db80bd65e50ac00d3da0019fed
Birkir Reynisson <birkirvr@gmail.com>
2024-09-18 20:09:40 +0000

    Fyrsta commit í tíma hjá NTV
    yes

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ntvTimi01
+A sample command-line application providing basic argument parsing with an entrypoint in `bin/`.
---
+++ bin/hello_world.dart
@@ -0,0 +1,5 @@
+void main() {
+  print("hello world");
+  String myName = "Birkir Reynisson";
+  print("What is my name?: $myName");
+}
